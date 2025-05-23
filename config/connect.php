a2615721039430bc26baa2b99e7e013bd93283dd
Hatenaa <121756340+Hatenaa@users.noreply.github.com>
2025-05-23 19:22:21 +0200

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -3,7 +3,7 @@
 try {
 
     global $db;
-    $db = new PDO('mysql:host=localhost;dbname=ecoride', 'root', 'root');
+    $db = new PDO('mysql:host=ecoride-khec.onrender.com;dbname=ecoride', 'root', 'root');
     $db->exec('SET NAMES "UTF8"');
 
 } catch (PDOException $e){
